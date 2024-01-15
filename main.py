7506b7894c169701653f60179e0e91178bb29ec6
Alexandre Arruda <alexandre.monteiro.bec@gmail.com>
2024-01-14 23:03:33 -0300

    add predict route

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
-from typing import Union
-
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+import tensorflow as tf
+import tensorflow_decision_forests as tfdf
+import numpy as np
+import utils
 
 app = FastAPI()
+model = tf.keras.models.load_model('model')
+
+
+class LandmarksList(BaseModel):
+  landmarks: List[float]
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def hello_word():
+  return {"response": "hello word"}
 
+
+@app.post("/predict/")
+def predict(landmarks_list: LandmarksList):
+  input_data = utils.landmarks_to_input_data(landmarks_list.landmarks)
+  prediction = model.predict(input_data)
+  signal = utils.SIGNALS[np.argmax(prediction)]
+
+  return {"signal": signal}
