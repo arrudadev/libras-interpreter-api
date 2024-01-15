0fb955b84eb7a82d3607e7dbdd9911d525218e0b
Alexandre Arruda <alexandre.monteiro.bec@gmail.com>
2024-01-15 02:22:35 -0300

    configure cors

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
 import tensorflow as tf
@@ -8,6 +9,14 @@ import utils
 
 app = FastAPI()
 model = tf.keras.models.load_model('model')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class LandmarksList(BaseModel):
